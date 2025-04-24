8eb406b8135f78ac57dbe886bd5e6b3dc69a4531
Tat Dat <phungdat@hongthaix.com>
2025-04-24 20:06:01 +0700

    format with black

--- src/config.py
+++ src/config.py
@@ -23,14 +23,17 @@ print(f"API_PORT: {API_PORT}")
 # --- Cấu hình Huấn luyện & Dữ liệu ---
 N_SAMPLES = int(os.getenv("N_SAMPLES", "1000"))
 N_FEATURES = int(os.getenv("N_FEATURES", "10"))
-N_CLASSES = int(os.getenv("N_CLASSES", "2"))    
+N_CLASSES = int(os.getenv("N_CLASSES", "2"))
 RANDOM_STATE_DATA = int(os.getenv("RANDOM_STATE_DATA", "42"))
 TEST_SIZE = float(os.getenv("TEST_SIZE", "0.2"))
 RANDOM_STATE_SPLIT = int(os.getenv("RANDOM_STATE_SPLIT", "123"))
 
 MODEL_TYPE = os.getenv("MODEL_TYPE", "LogisticRegression")
 HYPERPARAMS_TO_TUNE = {
-    "LogisticRegression": {"C": [0.5, 1.0, 5.0, 20.0], "solver": ["liblinear"]}, # Thay đổi danh sách giá trị C
+    "LogisticRegression": {
+        "C": [0.5, 1.0, 5.0, 20.0],
+        "solver": ["liblinear"],
+    },  # Thay đổi danh sách giá trị C
     "RandomForestClassifier": {"n_estimators": [50, 100], "max_depth": [None, 10]},
 }
 MODEL_PARAMS = HYPERPARAMS_TO_TUNE.get(MODEL_TYPE, {})
